5d6f3c021254d375993ec70913f1d4fa4fde9528
Nicybuchii <elloquepavos@gmail.com>
2025-09-03 18:00:33 -0300

    primer cambio juntos

--- ejercicio1.cpp
+++ ejercicio1.cpp
@@ -5,7 +5,73 @@
 
 using namespace std;
 
+struct NodoAvl{
+    int puntaje;
+    int id;
+    int altura;
+    string nombre;
+    NodoAvl* izq;
+    NodoAvl* der;
+    NodoAvl(int _puntaje, int _id, string _nombre):puntaje(_puntaje), id(_id), altura(1), nombre(_nombre), izq(NULL), der(NULL){}
+};
+
+class Avl{
+    NodoAvl* raiz;
+    int cantidadTotal;
+    NodoAvl* top1;
+    int alturaSegura(NodoAvl* nodo) {
+        if(nodo == NULL) return 0;
+        return nodo->altura;
+    }
+
+    void actualizarAltura(NodoAvl* nodo) {
+        if(nodo == NULL) return;
+        nodo->altura = 1 + max(alturaSegura(nodo->izq), alturaSegura(nodo->der));
+    }
+
+    int getBalance(NodoAvl * nodo) {
+        if(nodo == NULL) return 0;
+        return alturaSegura(nodo->izq) - alturaSegura(nodo->der);
+    }
+
+    void rotacionDerecha(NodoAvl*& B){
+        NodoAvl* A=B->izq;
+        NodoAvl* T2=A->der;
+        B->izq=T2;
+        A->der=B;
+        actualizarAltura(B);
+        actualizarAltura(A);
+        B=A;
+    }
+
+    void rotacionIzquierda(NodoAvl*& A){
+        NodoAvl* B = A->der;
+        NodoAvl*T2=B->izq; 
+        A->der = T2;
+        B->izq = A;
+        actualizarAltura(A);
+        actualizarAltura(B);
+        A = B;
+    }
+
+    bool insertarRec(NodoAvl*& raiz, int id, string nombre, int puntaje){
+        if (raiz==NULL){
+            raiz=new NodoAvl(puntaje, id, nombre);
+            if(cantidadTotal==0){
+                this->top1=raiz;
+            }else{
+                if (this->top1->puntaje<puntaje || (this->top1->puntaje==puntaje && this->top1->id>id)) this->top1=raiz;
+            }
+            this->cantidadTotal++;
+        }
+    }
+    public:
+
+    void insertar(int id, string nombre, int puntaje){
+        
+    }
+};
 int main()
 {
-    return 0;
+    
 }
