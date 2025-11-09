2b5f0ff9fa210399fbe2ce51477bebd75aa4d8ff
PdMoraesMvd <MM307164@fi365.ort.edu.uy>
2025-11-09 13:21:23 -0300

    Ejercicio 7 y 9 terminados

--- ejercicio7.cpp
+++ ejercicio7.cpp
@@ -2,11 +2,159 @@
 #include <string>
 #include <iostream>
 #include <limits>
-
 using namespace std;
+
+struct nodoHash
+{
+    string nombre;
+    int posicion;
+    nodoHash *sig;
+};
+
+class tablaHash
+{
+private:
+    nodoHash **tabla;
+    int capacidad;
+
+    int h1(string clave)
+    { // primera funcion de hash. Sacada del curso teórico (ver documentacion)
+        int h = 0;
+        for (int i = 0; i < clave.length(); i++)
+        {
+            h = 31 * h + int(clave[i]);
+        }
+        return abs(h);
+    }
+
+public:
+    tablaHash(int cantidadElementos)
+    {
+        capacidad = cantidadElementos * 1.5;
+        tabla = new nodoHash *[capacidad];
+        for (int i = 0; i < capacidad; i++)
+        {
+            tabla[i] = NULL;
+        }
+    }
+    void agregarElemento(string nombre, int posicion)
+    {
+        // ya sabemos que no se va a repetir nunca un nombre por letra, tambien que el factor de carga no va a pasar el 0.5
+        int pos = h1(nombre) % capacidad;
+        nodoHash *nodo = new nodoHash;
+        nodo->nombre = nombre;
+        nodo->posicion = posicion;
+        if (tabla[pos] == NULL)
+        {
+            nodo->sig = NULL;
+            tabla[pos] = nodo;
+        }
+        else
+        {
+            nodo->sig = tabla[pos];
+            tabla[pos] = nodo;
+        }
+    }
+    int buscarElemento(string nombre)
+    {
+        // nunca vamos a buscar un elemento que no exista asi que no hay que hacer ese chequeo
+        int pos = h1(nombre) % capacidad;
+        nodoHash *aux = tabla[pos];
+        while (aux != NULL && aux->nombre != nombre)
+            aux = aux->sig;
+        return aux->posicion;
+    }
+};
+
+long long contarInversionesMitad(int *a, int inicio, int mitad, int final)
+{
+    int *aux = new int[final - inicio + 1];
+    long long ret = 0;
+    int i = inicio;
+    int j = mitad + 1;
+    int k = 0;
+    while (i <= mitad && j <= final)
+    {
+        if (a[i] <= a[j])
+        {
+            aux[k] = a[i];
+            k++;
+            i++;
+        }
+        else
+        {
+            aux[k] = a[j];
+            k++;
+            j++;
+            ret += (mitad - i + 1); // todos los que quedan en la izquierda son mayores
+        }
+    }
+
+    // copiar lo que quede en la mitad izquierda
+    while (i <= mitad)
+    {
+        aux[k] = a[i];
+        k++;
+        i++;
+    }
+
+    // copiar lo que quede en la mitad derecha
+    while (j <= final)
+    {
+        aux[k] = a[j];
+        k++;
+        j++;
+    }
+
+    // poner aux en el array original p que quede ordenado
+    for (int t = 0; t < k; t++)
+    {
+        a[inicio + t] = aux[t];
+    }
+
+    delete[] aux;
+
+    return ret;
+}
+
+long long contarInversiones(int *a, int inicio, int final)
+{
+    // caso base
+    if (inicio >= final)
+        return 0;
+
+    // dividir
+    int mitad = (inicio + final) / 2;
+    long long cantInversionesIzq = contarInversiones(a, inicio, mitad);
+    long long cantInversionesDer = contarInversiones(a, mitad + 1, final);
+    long long cantInversionesInter = contarInversionesMitad(a, inicio, mitad, final);
+
+    // merge
+    return cantInversionesIzq + cantInversionesDer + cantInversionesInter;
+}
 
 int main()
 {
-    // TODO
+    int N;
+    cin >> N;
+    int *rankingAyudante = new int[N];
+    tablaHash tabla(N);
+    for (int i = 0; i < N; i++)
+    {
+        string nom;
+        cin >> nom;
+        tabla.agregarElemento(nom, i);
+    }
+
+    for (int i = 0; i < N; i++)
+    {
+        string nom;
+        cin >> nom;
+        int pos = tabla.buscarElemento(nom);
+        rankingAyudante[i] = pos;
+    }
+    long long total = contarInversiones(rankingAyudante, 0, N - 1);
+    cout << total << endl;
+    delete[] rankingAyudante;
     return 0;
 }
--- ejercicio9.cpp
+++ ejercicio9.cpp
@@ -2,11 +2,79 @@
 #include <string>
 #include <iostream>
 #include <limits>
-
 using namespace std;
+
+int max(int a, int b)
+{
+    if (a > b)
+        return a;
+    return b;
+}
+
+void mejorPuntaje(int capacidadMB, int capacidadLineas, int cantidadObjetos, int *pesos, int *puntajes, int *cantLineas)
+{
+
+    int ***tab = new int **[cantidadObjetos + 1]; // fila de 0 caso base
+    for (int ejercicioActual = 0; ejercicioActual <= cantidadObjetos; ejercicioActual++)
+    {
+        tab[ejercicioActual] = new int *[capacidadMB + 1](); // columna de 0 caso base
+        for (int capacidadActual = 0; capacidadActual <= capacidadMB; capacidadActual++)
+        {
+            tab[ejercicioActual][capacidadActual] = new int[capacidadLineas + 1]();
+        }
+    }
+
+    for (int ejercicioActual = 1; ejercicioActual <= cantidadObjetos; ejercicioActual++)
+    {
+        int pesoEjercicioActual = pesos[ejercicioActual - 1];
+        int puntajeEjercicioActual = puntajes[ejercicioActual - 1];
+        int lineasEjercicioActual = cantLineas[ejercicioActual - 1];
+        for (int capacidadMBActual = 0; capacidadMBActual <= capacidadMB; capacidadMBActual++)
+        {
+
+            for (int capacidadLineasActual = 0; capacidadLineasActual <= capacidadLineas; capacidadLineasActual++)
+            {
+                // El ejercicio actual no entra en la capacidad actual
+                if (pesoEjercicioActual > capacidadMBActual || lineasEjercicioActual > capacidadLineasActual)
+                {
+                    // valor maximo sin usar el ejercicio
+                    tab[ejercicioActual][capacidadMBActual][capacidadLineasActual] = tab[ejercicioActual - 1][capacidadMBActual][capacidadLineasActual];
+                }
+                else
+                {
+                    int valorDeUsarElObjeto = puntajeEjercicioActual + tab[ejercicioActual - 1][capacidadMBActual - pesoEjercicioActual][capacidadLineasActual - lineasEjercicioActual];
+                    int valorDeNoUsarElObjeto = tab[ejercicioActual - 1][capacidadMBActual][capacidadLineasActual];
+                    tab[ejercicioActual][capacidadMBActual][capacidadLineasActual] = max(valorDeUsarElObjeto, valorDeNoUsarElObjeto);
+                }
+            }
+        }
+    }
+    cout << tab[cantidadObjetos][capacidadMB][capacidadLineas] << endl;
+}
 
 int main()
 {
-    // TODO
+    int cantidadObjetos;
+    cin >> cantidadObjetos;
+    int capacidadMB;
+    int capacidadLineas;
+    cin >> capacidadMB;
+    cin >> capacidadLineas;
+    int *puntajes = new int[cantidadObjetos];
+    int *pesos = new int[cantidadObjetos];
+    int *lineas = new int[cantidadObjetos];
+    for (int i = 0; i < cantidadObjetos; i++)
+    {
+        int peso;
+        int linea;
+        int puntaje;
+        cin >> peso;
+        cin >> linea;
+        cin >> puntaje;
+        puntajes[i] = puntaje;
+        pesos[i] = peso;
+        lineas[i] = linea;
+    }
+    mejorPuntaje(capacidadMB, capacidadLineas, cantidadObjetos, pesos, puntajes, lineas);
     return 0;
 }
